cc1e8442aaaf9bf78e57cd6a8303445e20c6c294
Md Istiak Ahmed <istinishat@gmail.com>
2022-08-21 23:36:31 +0600

    Restructure front-end code

---
+++ frontend/src/views/component/Product/ProductDetails.js
@@ -0,0 +1,205 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./ProductDetails.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails, newReview } from "../../../actions/productAction";
+import { useParams } from "react-router-dom";
+import ReviewCard from "./ReviewCard";
+import Loader from "../layout/loader/Loader";
+import MetaData from "../layout/MetaData";
+import { addItemsToCart } from "../../../actions/cartAction";
+import Rating from "@mui/material/Rating";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+import { NEW_REVIEW_RESET } from "../../../constants/productConstants";
+
+const ProductDetails = () => {
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  const { success, error: reviewError } = useSelector(
+    (state) => state.newReview
+  );
+
+  let options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
+
+  const [quantity, setQuantity] = useState(1);
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+
+  const increaseQuantity = () => {
+    if (product.Stock <= quantity) return;
+    const qty = quantity + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity === 1) return;
+    const qty = quantity - 1;
+    setQuantity(qty);
+  };
+
+  const submitReviewToggle = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+
+  const addToCartHandler = () => {
+    dispatch(addItemsToCart(id, quantity));
+    toast("Item added to card");
+  };
+
+  const reviewSubmitHandler = () => {
+    const myForm = new FormData();
+
+    myForm.set("rating", rating);
+    myForm.set("comment", comment);
+    myForm.set("productId", id);
+
+    dispatch(newReview(myForm));
+
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+
+    if (success) {
+      alert("Review Submitted Successfully");
+      dispatch({ type: NEW_REVIEW_RESET });
+    }
+    options = { ...options, value: product.ratings };
+  }, [dispatch, id, success, reviewError]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={`${product.name}`} />
+
+          <div className="ProductDetails">
+            <div>
+              {/*Istiak : Put Carousal HERE instead of single image */}
+              {product.images && (
+                <img
+                  className="CarouselImage"
+                  src={product.images[0].url}
+                  alt={product.name}
+                />
+              )}
+            </div>
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <Rating {...options} />
+                <span className="detailsBlock-2-span">
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`৳${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button onClick={decreaseQuantity}>-</button>
+                    <input readOnly value={quantity} type="number" />
+                    <button onClick={increaseQuantity}>+</button>
+                  </div>
+                  <button
+                    disabled={product.Stock < 1 ? true : false}
+                    onClick={addToCartHandler}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+              </div>
+
+              <div className="detailsBlock-4">
+                Description: <p>{product.description}</p>
+              </div>
+
+              <button onClick={submitReviewToggle} className="submitReview">
+                Submit Review
+              </button>
+            </div>
+          </div>
+
+          <h3 className="reviewsHeading">REVIEWS</h3>
+
+          <Dialog
+            aria-labelledby="simple-dialog-title"
+            open={open}
+            onClose={submitReviewToggle}
+          >
+            <DialogTitle>Submit Review</DialogTitle>
+            <DialogContent className="submitDialog">
+              <Rating
+                onChange={(e) => setRating(e.target.value)}
+                value={rating}
+                size="large"
+              />
+
+              <textarea
+                className="submitDialogTextArea"
+                cols="30"
+                rows="5"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              ></textarea>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={submitReviewToggle} color="secondary">
+                Cancel
+              </Button>
+              <Button onClick={reviewSubmitHandler} color="primary">
+                Submit
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map((review, idx) => (
+                  <ReviewCard key={idx} review={review} />
+                ))}
+            </div>
+          ) : (
+            <p className="noReviews"> No Reviews Yet</p>
+          )}
+        </Fragment>
+      )}
+      <ToastContainer />
+    </Fragment>
+  );
+};
+
+export default ProductDetails;
